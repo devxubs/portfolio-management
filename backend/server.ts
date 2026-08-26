d2d2610bbfbddf51a624283a2c35791e7b152780
Jubayer Ahmed <devxub@gmail.com>
2026-08-27 02:21:38 +0600

    working

--- backend/server.ts
+++ backend/server.ts
@@ -13,7 +13,7 @@ const PORT = Number(process.env.PORT) || 5000;
 // CORS
 app.use(
    cors({
-      origin: process.env.FRONTEND_URL,
+      origin: "https://portfolio-management-wheat-six.vercel.app/",
       credentials: true,
    }),
 );
